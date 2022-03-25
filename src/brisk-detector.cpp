3e7049bb298247928a87597a61618bbd0eb95b76
emirdeliz <emirdeliz@gmail.com>
2022-03-25 15:32:57 -0300

    work in progress

--- src/brisk-detector.cpp
+++ src/brisk-detector.cpp
@@ -75,26 +75,33 @@ vector<DMatch>get_best_matches(vector<DMatch> matches, Mat distances)
 	return bestMatches;
 }
 
-// void show_result(Mat img1, Mat img2, vector<KeyPoint> keyImg1, vector<KeyPoint> keyImg2, vector<DMatch> bestMatches)
-// {
-// 	Mat result;
-// 	drawMatches(img1, keyImg1, img2, keyImg2, bestMatches, result);
-// 	imshow("Result", result);
-// }
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	void show_result(Mat img1, Mat img2, vector<KeyPoint> keyImg1, vector<KeyPoint> keyImg2, vector<DMatch> bestMatches)
+	{
+		Mat result;
+		drawMatches(img1, keyImg1, img2, keyImg2, bestMatches, result);
+		imshow("Result", result);
+	}
 
-// int main(int argc, char **argv)
-// {
-// 	vector<Mat> images = get_images(argc, argv);
-// 	Mat img1 = images[0];
-// 	Mat img2 = images[1];
+	int main(int argc, char **argv)
+	{
+		vector<Mat> images = get_images(argc, argv);
+		Mat img1 = images[0];
+		Mat img2 = images[1];
 
-// 	auto [keyImg1, keyImg2, descImg1, descImg2, matches] = match_image(img1, img2);
-// 	Ptr<DescriptorMatcher> descriptorMatcher = DescriptorMatcher::create("BruteForce");
+		auto [keyImg1, keyImg2, descImg1, descImg2, matches] = match_image(img1, img2);
+		Ptr<DescriptorMatcher> descriptorMatcher = DescriptorMatcher::create("BruteForce");
 
-// 	Mat distances = calc_distance(matches);
-// 	vector<DMatch> bestMatches = get_best_matches(matches, distances);
+		Mat distances = calc_distance(matches);
+		vector<DMatch> bestMatches = get_best_matches(matches, distances);
 
-// 	show_result(img1, img2, keyImg1, keyImg2, bestMatches);
-// 	waitKey();
-// 	return 0;
-// }
+		show_result(img1, img2, keyImg1, keyImg2, bestMatches);
+		waitKey();
+		return 0;
+	}
+#ifdef __cplusplus
+}
+#endif
