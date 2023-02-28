d84d37715a47a3bbacf3d7661734860c885ec5b0
Emir Marques <emirdeliz@gmail.com>
2023-02-28 11:26:40 -0300

    feat: Work in progress

--- src/brisk-detector.cpp
+++ src/brisk-detector.cpp
@@ -1,107 +1,34 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/features2d.hpp>
-#include <opencv2/highgui.hpp>
-#include <vector>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
-static void help(char *argv[])
+int main(int argc, char *argv[])
 {
-	cout << "\n This program demonstrates how to detect compute and match BRISK descriptors \n"
-					"Usage: \n  "
-			 << argv[0] << " --image1=<image1(doctor-house.png as default)> --image2=<image2(doctor-house-tongue.png as default)>\n"
-										 "Press a key when image window is active to change algorithm or descriptor";
-}
+	Mat img1 = imread("../assets/doctor-house.png");
+	Mat img2 = imread("../assets/doctor-house-tongue.png");
 
-vector<Mat> get_images(int argc, char **argv)
-{
-	cv::CommandLineParser parser(
-			argc,
-			argv,
-			"{ @image1 | ../assets/doctor-house.png | }"
-			"{ @image2 | ../assets/doctor-house-tongue.png | }");
-
-	String fileName1 = samples::findFile(parser.get<string>(0));
-	String fileName2 = samples::findFile(parser.get<string>(1));
-	Mat img1 = imread(fileName1, IMREAD_GRAYSCALE);
-	Mat img2 = imread(fileName2, IMREAD_GRAYSCALE);
-	return vector<Mat>{img1, img2};
-}
-
-std::tuple<vector<KeyPoint>, vector<KeyPoint>, Mat, Mat, vector<DMatch>> match_image(Mat img1, Mat img2)
-{
-	// BRISK (Binary Robust Invariant Scalable Keypoints)
-	Ptr<Feature2D> detector = BRISK::create();
-	vector<KeyPoint> keyImg1, keyImg2;
-	Mat descImg1, descImg2;
-	
-	detector->detect(img1, keyImg1, Mat());
-	// and compute their descriptors with method  compute
-	detector->compute(img1, keyImg1, descImg1);
-	// or detect and compute descriptors in one step
-	detector->detectAndCompute(img2, Mat(), keyImg2, descImg2);
-
-	vector<DMatch> matches;
-	Ptr<DescriptorMatcher> descriptorMatcher = DescriptorMatcher::create("BruteForce");
-	descriptorMatcher->match(descImg1, descImg2, matches, Mat());
-	return std::make_tuple(keyImg1, keyImg2, descImg1, descImg2, matches);
-}
-
-Mat calc_distance(vector<DMatch> matches)
-{
-	int nbMatch = int(matches.size());
-	Mat tab(nbMatch, 1, CV_32F);
-
-	for (int i = 0; i < nbMatch; i++)
+	if (img1.empty() || img2.empty())
 	{
-		tab.at<float>(i, 0) = matches[i].distance;
-	}
-	return tab;
-}
-
-int EMSCRIPTEN_KEEPALIVE;
-vector<DMatch>get_best_matches(vector<DMatch> matches, Mat distances)
-{
-	Mat index;
-	sortIdx(distances, index, SORT_EVERY_COLUMN + SORT_ASCENDING);
-	vector<DMatch> bestMatches;
-	for (int i = 0; i < 30; i++)
-	{
-		bestMatches.push_back(matches[index.at<int>(i, 0)]);
-	}
-	return bestMatches;
-}
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-	void show_result(Mat img1, Mat img2, vector<KeyPoint> keyImg1, vector<KeyPoint> keyImg2, vector<DMatch> bestMatches)
-	{
-		Mat result;
-		drawMatches(img1, keyImg1, img2, keyImg2, bestMatches, result);
-		imshow("Result", result);
+		std::cout << "Erro ao carregar a imagem" << std::endl;
+		return -1;
 	}
 
-	int main(int argc, char **argv)
-	{
-		vector<Mat> images = get_images(argc, argv);
-		Mat img1 = images[0];
-		Mat img2 = images[1];
+	Mat result;
+	int result_cols = img1.cols - img2.cols + 1;
+	int result_rows = img1.rows - img2.rows + 1;
+	result.create(result_rows, result_cols, CV_32FC1);
 
-		auto [keyImg1, keyImg2, descImg1, descImg2, matches] = match_image(img1, img2);
-		Ptr<DescriptorMatcher> descriptorMatcher = DescriptorMatcher::create("BruteForce");
+	matchTemplate(img1, img2, result, TM_CCOEFF_NORMED);
+	Point max_loc;
+	minMaxLoc(result, NULL, NULL, NULL, &max_loc);
 
-		Mat distances = calc_distance(matches);
-		vector<DMatch> bestMatches = get_best_matches(matches, distances);
+	rectangle(img1, max_loc, Point(max_loc.x + img2.cols, max_loc.y + img2.rows), Scalar(0, 0, 255), 2);
+	imshow(to_string(max_loc.y + img2.rows), img1);
 
-		show_result(img1, img2, keyImg1, keyImg2, bestMatches);
-		waitKey();
-		return 0;
-	}
-#ifdef __cplusplus
+	// X: 464
+	// Y: 773
+
+	waitKey(0);
 }
-#endif
